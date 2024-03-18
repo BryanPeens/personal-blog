8d63f65098de70d044f8311c33be918f519280ef
Bryan Peens <peensbryan1@gmail.com>
2024-03-17 23:29:31 -0400

    updated blog page and logic, and fixed slider

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    const postContainer = document.querySelector('#post-container');
+    const postContainer = document.querySelector('#blog-container');
     const lineDiv = document.querySelector('#line-div');
     const usernameField = document.querySelector('#username');
     const titleField = document.querySelector('#title');
@@ -11,24 +11,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Add event listener for 'click' event
     checkbox.addEventListener('click', function() {
-        if (this.checked) {
+        if (this.checked) { 
             console.log('Checkbox is checked');
-            document.body.style.backgroundColor = "black";
-            // postContainer.color = "white";
-            // lineDiv.style.borderColor = "white";
-            // titleField.color = "white";
-            // contentField.color = "white";
-            // usernameField.color = "white";
-            // postContainer.borderColor = "white";
+            document.body.style.backgroundColor = "black"; // Change background color
+            postContainer.style.color = "white";
+            lineDiv.style.borderColor = "white";
+            titleField.style.color = "white";
+            contentField.style.color = "white";
+            usernameField.style.color = "white";
+            postContainer.style.borderColor = "white";
         } else {
             console.log('Checkbox is not checked');
             document.body.style.backgroundColor = "white";
-            // postContainer.style.color = "black"; // Should be the text color
-            // lineDiv.style.borderColor = "black";
-            // titleField.style.color = "black";
-            // contentField.style.color = "black";
-            // usernameField.style.color = "black";
-            // postContainer.style.borderColor = "black";
+            postContainer.style.color = "black"; // text color
+            lineDiv.style.borderColor = "black";
+            titleField.style.color = "black";
+            contentField.style.color = "black";
+            usernameField.style.color = "black";
+            postContainer.style.borderColor = "black";
         }
     });
 });
